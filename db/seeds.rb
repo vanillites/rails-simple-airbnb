6a9b746ee04c390e49cbac7af869da8ae04c24f1
Julian Harris <vanillites@protonmail.com>
2022-07-30 14:06:02 +0800

    create Flat model and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "🗑 Clearing DB..."
+Flat.destroy_all
+
+puts "🌱 Seeding data..."
+i = 0
+
+20.times do
+  Flat.create!(
+    name: "#{123 + i} Main St",
+    address: Faker::Address.full_address,
+    description: "This is an apartment",
+    price_per_night: (100..1000).to_a.sample,
+    number_of_guests: (1..5).to_a.sample
+  )
+  i += 1
+end
